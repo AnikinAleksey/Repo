a030dda468ab0b6de2a5e163d2691f69783e9c4e
AnikinAV <ankinaleksey2003@mail.ru>
2024-05-01 21:19:52 +0400

    добавлен класс связного списка List

--- Adapter/Adapter.cpp
+++ Adapter/Adapter.cpp
@@ -11,7 +11,66 @@ public:
     virtual bool isEmpty() const = 0;
     virtual ~Stack() {}
 };
-
+//Класс узла списка
+template <typename T>
+class Node  {
+public:
+    T data;
+    Node <T>* next;
+    Node(T value) : data(value), next(nullptr) {}
+};
+// Адаптируемый класс "Связный список"
+template <typename T>
+class List {
+private:
+    Node<T>* head;
+public:
+    List() : head(nullptr) {}
+    void addToEnd(T element) {
+        Node<T>* newNode = new Node<T>(element);
+        if (head == nullptr) {
+            head = newNode;
+        }
+        else {
+            Node<T >* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+    T removeFromEnd() {
+        if (head != nullptr) {
+            if (head->next == nullptr) {
+                T tmp = head->data;
+                delete head;
+                head = nullptr;
+                return tmp;
+            }
+            else {
+                Node<T>* current = head;
+                while (current->next->next != nullptr) {
+                    current = current->next;
+                }
+                T tmp = current->next->data;
+                delete current->next;
+                current->next = nullptr;
+                return tmp;
+            }
+        }
+    }
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+    ~List() {
+        Node<T>* current = head;
+        while (current != nullptr) {
+            Node<T>* temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
+};
 
 
 
