73da302a37fbb5d62852d51b58f7b413ba21af15
AnikinAV <ankinaleksey2003@mail.ru>
2024-05-01 21:18:50 +0400

    добавлен класс Stack

---
+++ Adapter/Adapter.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+//Целевой класс "Стек"
+template <typename T>
+class Stack {
+public:
+    Stack() {};
+    virtual void push(T element) = 0;
+    virtual T pop() = 0;
+    virtual bool isEmpty() const = 0;
+    virtual ~Stack() {}
+};
+
+
+
+
